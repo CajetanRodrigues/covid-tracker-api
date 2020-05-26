dbb7453fd248c0a313d0c9296ee9ace54e333788
Cajetan <rigrod1999@gmail.com>
2020-05-26 10:07:02 +0530

    Update host

--- app.py
+++ app.py
@@ -293,5 +293,5 @@ def ret():
         deaths.append(x["deaths"])
     return json.dumps(deaths)
 if __name__ == '__main__':  
-    app.run(host='127.0.0.1',port=8082,debug = True)
+    app.run(host='0.0.0.0',port=8082,debug = True)
 
